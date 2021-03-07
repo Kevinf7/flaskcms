47886566db734335d9e7584b7a0929d367ad08e6
Kevin Foong <kevin_foong@yahoo.com>
2021-03-07 20:38:41 +1100

    minor

--- config.py
+++ config.py
@@ -8,15 +8,16 @@ load_dotenv(basedir / '.env')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'random string'
-    #SERVER_NAME = 'flaskcms.pythonanywhere.com'
-    SERVER_NAME = 'localhost:5000'
-
-    # Dev only so browser doesnt cache for CSS
+    
     if (os.environ.get('FLASK_ENV') == 'development'):
+        # Dev only so browser doesnt cache for CSS
         SEND_FILE_MAX_AGE_DEFAULT = 0
-    # auto reload template without needing to restart Flask
-    TEMPLATES_AUTO_RELOAD = True
-
+        # auto reload template without needing to restart Flask
+        TEMPLATES_AUTO_RELOAD = True
+        SERVER_NAME = 'localhost:5000'
+    else:
+        SERVER_NAME = 'flaskcms.pythonanywhere.com'
+    
     #####
 
     # Database settings
