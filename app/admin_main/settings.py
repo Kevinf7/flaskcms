cc6a9762a462174a9399e4652f44f3105fb957b2
Kevin Foong <kevin_foong@yahoo.com>
2021-04-11 15:51:49 +1000

    minor

--- app/admin_main/settings.py
+++ app/admin_main/settings.py
@@ -6,7 +6,10 @@ def set_settings():
     setting=db.session.query(SiteSetting).first()
     current_app.config['SENDGRID_KEY'] = setting.sendgrid_key
     current_app.config['MAIL_FROM'] = setting.mail_from
-    current_app.config['MAIL_ADMINS'] = setting.mail_admins.split(' ')
+    if setting.mail_admins:
+        current_app.config['MAIL_ADMINS'] = setting.mail_admins.split(' ')
+    else:
+        current_app.config['MAIL_ADMINS'] = ''
     current_app.config['USER_POSTS_PER_PAGE'] = setting.posts_per_page
     current_app.config['GOOGLE_MAP_KEY'] = setting.google_map_key
     current_app.config['RECAPTCHA_KEY'] = setting.recaptcha_key
